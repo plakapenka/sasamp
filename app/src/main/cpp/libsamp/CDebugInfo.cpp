dc6568349412fc5f5a59173aa616556b61831e77
plakapenka <plakapenok1@gmail.com>
2022-10-18 13:58:38 +0300

    fix отображения занимаемой памяти

--- app/src/main/cpp/libsamp/CDebugInfo.cpp
+++ app/src/main/cpp/libsamp/CDebugInfo.cpp
@@ -12,56 +12,60 @@
 #include "CSettings.h"
 #include "util/armhook.h"
 
-extern CGUI *pGUI;
-extern CChatWindow *pChatWindow;
+extern CGUI* pGUI;
 extern CGame *pGame;
-extern CSettings *pSettings;
+extern CChatWindow* pChatWindow;
 
 uint32_t CDebugInfo::uiStreamedPeds = 0;
 uint32_t CDebugInfo::uiStreamedVehicles = 0;
 uint32_t CDebugInfo::m_uiDrawDebug = 0;
+uint32_t CDebugInfo::m_uiDrawFPS = 0;
 uint32_t CDebugInfo::m_dwSpeedMode = 0;
 uint32_t CDebugInfo::m_dwSpeedStart = 0;
+
+void CDebugInfo::ToggleDebugDraw()
+{
+	m_uiDrawDebug ^= 1;
+}
+
+void CDebugInfo::SetDrawFPS(uint32_t bDraw)
+{
+	m_uiDrawFPS = bDraw;
+}
 
 void CDebugInfo::Draw()
 {
 	char szStr[30];
-	char szStrPos[256];
 	char szStrMem[256];
+	char szStrPos[256];
 	ImVec2 pos;
-	ImVec2 pospos;
-	ImVec2 pospospos;
-	if (pSettings->GetReadOnly().szDebug == 1)
+	if (m_uiDrawDebug || m_uiDrawFPS)
 	{
-		float *pFPS = (float *)(g_libGTASA + 0x00608E00);
-		snprintf(&szStr[0], 30, "FPS: %.1f", *pFPS);
-		pos = ImVec2(pGUI->ScaleX(25.0f), pGUI->ScaleY(465.0f - pGUI->GetFontSize()));
-		pospos = ImVec2(pGUI->ScaleX(25.0f), pGUI->ScaleY(465.0f));
-		pospospos = ImVec2(pGUI->ScaleX(25.0f), pGUI->ScaleY(465.0f + pGUI->GetFontSize()));
+		float* pFPS = (float*)(g_libGTASA + 0x00608E00);
+		snprintf(&szStr[0], 30, "FPS: %.0f", *pFPS);
+		pos = ImVec2(pGUI->ScaleX(40.0f), pGUI->ScaleY(1080.0f - pGUI->GetFontSize() * 10));
 
 		pGUI->RenderText(pos, (ImU32)0xFFFFFFFF, true, &szStr[0]);
 
-		int meminfo = 0;
-		snprintf(&szStrMem[0], 256, "MEM: %d/%d MB", meminfo, meminfo);
+		uint32_t msUsed = *(uint32_t*)(g_libGTASA + 0x0067067C);
+		uint32_t msAvailable = *(uint32_t*)(g_libGTASA + 0x005DE734);
+		float percentUsed = ((float)msUsed/(float)msAvailable)*100;
+		snprintf(&szStrMem[0], 256, "MEM: %.1f/%.1f (%.1f %%)",
+				 (float)msUsed/100000,
+				 (float)msAvailable/100000,
+				 percentUsed
+				 );
+		pos = ImVec2(pGUI->ScaleX(40.0f), pGUI->ScaleY(1080.0f - pGUI->GetFontSize() * 9));
 
-		pGUI->RenderText(pospos, (ImU32)0xFFFFFFFF, true, &szStrMem[0]);
+		pGUI->RenderText(pos, (ImU32)0xFFFFFFFF, true, &szStrMem[0]);
 
 		if (pGame->FindPlayerPed()->m_pPed)
 		{
 			snprintf(&szStrPos[0], 256, "POS: %.2f, %.2f, %.2f", pGame->FindPlayerPed()->m_pPed->entity.mat->pos.X, pGame->FindPlayerPed()->m_pPed->entity.mat->pos.Y, pGame->FindPlayerPed()->m_pPed->entity.mat->pos.Z);
-			
-			pGUI->RenderText(pospospos, (ImU32)0xFFFFFFFF, true, &szStrPos[0]);
+			pos = ImVec2(pGUI->ScaleX(40.0f), pGUI->ScaleY(1080.0f - pGUI->GetFontSize() * 8));
+			pGUI->RenderText(pos, (ImU32)0xFFFFFFFF, true, &szStrPos[0]);
 		}
 	}
-
-#ifdef DEBUG_INFO_ENABLED
-
-	if (!m_uiDrawDebug)
-	{
-		return;
-	}
-
-#endif
 }
 
 void CDebugInfo::ApplyDebugPatches()
@@ -69,7 +73,7 @@ void CDebugInfo::ApplyDebugPatches()
 #ifdef DEBUG_INFO_ENABLED
 
 	UnFuck(g_libGTASA + 0x008B8018);
-	*(uint8_t *)(g_libGTASA + 0x008B8018) = 1;
+	*(uint8_t*)(g_libGTASA + 0x008B8018) = 1;
 	NOP(g_libGTASA + 0x00399EDA, 2);
 	NOP(g_libGTASA + 0x00399F46, 2);
 	NOP(g_libGTASA + 0x00399F92, 2);
@@ -90,7 +94,7 @@ void CDebugInfo::ToggleSpeedMode()
 	}
 }
 
-void CDebugInfo::ProcessSpeedMode(VECTOR *pVecSpeed)
+void CDebugInfo::ProcessSpeedMode(VECTOR* pVecSpeed)
 {
 	if (!m_dwSpeedMode)
 	{
