336745decfa0de3c55f078638d881cdd282347ac
ivanlikhvatov <ivanlikhvatov@icloud.com>
2022-11-08 02:14:12 +0400

    fix loading

--- app/src/main/java/com/liverussia/launcher/config/Config.java
+++ app/src/main/java/com/liverussia/launcher/config/Config.java
@@ -1,20 +1,22 @@
 package com.liverussia.launcher.config;
 
 public class Config {
-    public static final String APP_PATH = "/storage/emulated/0/Android/data/com.liverussia.cr/files/";
+
     public static final String GAME_PATH = "/storage/emulated/0/Android/data/com.liverussia.cr/files/";
     public static String PATH_DOWNLOADS = "/storage/emulated/0/Android/data/com.liverussia.cr/files/temp_downloads/";
     public static final String URL_FILES = "https://vl4sov.com/Download/game.zip";
     public static final String URL_FILES_UPDATE = "https://vl4sov.com/Download/upd.zip";
     public static final String URL_CLIENT = "https://vl4sov.com/Download/launcher.apk";
-    public static final String FILE_INFO_URL = "https://liverussia.online/downloads/files/files.json";
+
     public static final String URL_DONATE = "https://vl4sov.com/liverussia.online/Donate";
     public static final String GAME_DIR = "/storage/emulated/0/Android/data/";
+
+
+
+
+
     public static final String GAME_SETTINGS_DIRECTORY = "SAMP/settings.ini";
-
-
-
-
+    public static final String FILE_INFO_URL = "https://files.liverussia.online/downloads/files/files.json";
 
     public static final String DISCORD_URI = "https://discord.com/";
     public static final String VK_URI = "https://vk.com/";
