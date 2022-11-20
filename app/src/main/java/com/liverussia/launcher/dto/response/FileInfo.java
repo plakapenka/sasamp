86308a37d00f1db0621a57b44f2516f67e92fcb4
ivanlikhvatov <ivanlikhvatov@icloud.com>
2022-11-20 17:51:04 +0400

    add JsonIgnoreProperties

--- app/src/main/java/com/liverussia/launcher/dto/response/FileInfo.java
+++ app/src/main/java/com/liverussia/launcher/dto/response/FileInfo.java
@@ -1,10 +1,13 @@
 package com.liverussia.launcher.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FileInfo {
     private String link;
     private String path;
