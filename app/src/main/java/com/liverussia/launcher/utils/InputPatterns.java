5ef515f2392ce0c6b4bfd7d1a91ab18204a47263
ivanlikhvatov <ivanlikhvatov@icloud.com>
2022-11-28 23:27:04 +0400

    fix nickname validation

--- app/src/main/java/com/liverussia/launcher/utils/InputPatterns.java
+++ app/src/main/java/com/liverussia/launcher/utils/InputPatterns.java
@@ -1,7 +1,7 @@
 package com.liverussia.launcher.utils;
 
 public class InputPatterns {
-    public static final String NICKNAME_PATTERN = "^[A-Z][a-zA-Z]+_[A-Z][a-zA-Z]+$";
+    public static final String NICKNAME_PATTERN = "^[A-Z][a-z]+_[A-Z][a-z]+$";
     public static final String EMAIL_PATTERN = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
             + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
     public static final String STRING_WITH_DIGITS_PATTERN = ".*\\d+.*";
