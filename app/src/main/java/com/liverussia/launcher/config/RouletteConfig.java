028e647afcbcbef6d1cb214dcea8da6426698bd6
ivanlikhvatov <ivanlikhvatov@icloud.com>
2022-12-05 02:20:16 +0400

    refactoring

--- app/src/main/java/com/liverussia/launcher/config/RouletteConfig.java
+++
@@ -1,6 +0,0 @@
-package com.liverussia.launcher.config;
-
-public class RouletteConfig {
-//    public static final Long ROULETTE_DURATION_IN_MILLIS = 8000L;
-//    public static final int ROULETTE_COUNT_ELEMENTS_IN_ONE_SPIN = 104;
-}
