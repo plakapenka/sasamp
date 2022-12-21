2a23045edae588d7fa064f67e95f795b3f1e2fbe
plakapenka <plakapenok1@gmail.com>
2022-12-21 20:36:10 +0300

    новый сервер

--- app/src/main/cpp/libsamp/CServerManager.cpp
+++ app/src/main/cpp/libsamp/CServerManager.cpp
@@ -3,6 +3,7 @@
 #include <stdint.h>
 
 const CServerInstance::CServerInstanceEncrypted g_sEncryptedAddresses[MAX_SERVERS] = {
+		CServerInstance::create("94.250.248.153", 1, 20, 7777, false),
 	CServerInstance::create("46.174.49.47", 1, 20, 7788, false),	// москва
 	CServerInstance::create("46.174.49.47", 1, 20, 7825, false), 	// спб
 	CServerInstance::create("46.174.49.47", 1, 20, 7786, false),	// тест
