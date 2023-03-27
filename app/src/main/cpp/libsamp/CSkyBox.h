61558c2eff947fb1b6702329ecdbcecb8519f6e1
plakapenka <plakapenok1@gmail.com>
2023-03-27 23:26:00 +0300

    наброски скайбокса

---
+++ app/src/main/cpp/libsamp/CSkyBox.h
@@ -0,0 +1,40 @@
+// -- -- -- -- -- --
+// Created by Loony-dev © 2021
+// VK: https://vk.com/loonydev
+// -- -- -- -
+
+#pragma once
+
+#include "../main.h"
+
+class CObject;
+
+class CSkyBox {
+public:
+	static void Initialise();
+
+	static void Process();
+
+	static void SetRotSpeed(float mFRotSpeed);
+	static void SetTexture(const char *texName);
+
+	static bool IsNeedRender();
+
+	static CObject *GetSkyObject();
+
+private:
+	static CObject* CreateObjectScaled(int iModel, float fScale);
+
+	static void ReTexture();
+
+	static uintptr_t RwFrameForAllObjectsCallback(uintptr_t object, CObject* pObject);
+
+private:
+	static CObject* m_pSkyObject;
+
+	static bool m_bNeedRender;
+
+	static const char *m_TextureName;
+
+	static float m_fRotSpeed;
+};
