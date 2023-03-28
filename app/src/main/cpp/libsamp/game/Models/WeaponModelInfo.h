2295cd2e6c8df74049a4dd24d05abbc32eb6be15
plakapenka <plakapenok1@gmail.com>
2023-03-28 19:25:40 +0300

    CWeaponModelInfo

---
+++ app/src/main/cpp/libsamp/game/Models/WeaponModelInfo.h
@@ -0,0 +1,19 @@
+//
+// Created by plaka on 28.03.2023.
+//
+
+#ifndef LIVERUSSIA_WEAPONMODELINFO_H
+#define LIVERUSSIA_WEAPONMODELINFO_H
+
+
+#include "ClumpModelInfo.h"
+#include "../Enums/eWeaponType.h"
+
+class CWeaponModelInfo : public CClumpModelInfo {
+public:
+    eWeaponType m_weaponInfo;
+
+};
+
+
+#endif //LIVERUSSIA_WEAPONMODELINFO_H
