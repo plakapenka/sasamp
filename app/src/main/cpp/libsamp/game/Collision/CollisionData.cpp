07d135d365162f96891f3b68a82a7de7723287f9
plakapenka <plakapenok1@gmail.com>
2023-04-05 14:35:57 +0300

    CCollisionData

--- app/src/main/cpp/libsamp/game/Collision/CollisionData.cpp
+++ app/src/main/cpp/libsamp/game/Collision/CollisionData.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "CollisionData.h"
+
+CCollisionData::CCollisionData() {
+    char v1; // r2
+
+    m_nNoOfLines = 0;
+    m_nNoOfTriangles = 0;
+    m_nNoOfSpheres = 0;
+    m_pShadTriangleArray = nullptr;
+    v1 = *((uintptr_t *) this + 7);
+
+    m_pTriangleArray = nullptr;
+    m_nNoOfShadTriangles = 0LL;
+    m_pSphereArray = nullptr;
+    m_pLineArray = nullptr;
+    m_pTriCompressedShadVectorArray = nullptr;
+
+    *((uintptr_t *) this + 7) = v1 & 0xF8;
+}
--- app/src/main/cpp/libsamp/game/Collision/CollisionData.h
+++ app/src/main/cpp/libsamp/game/Collision/CollisionData.h
@@ -8,8 +8,16 @@
 
 #include "ColSphere.h"
 #include "ColLine.h"
+#include "ColBox.h"
+#include "ColTriangle.h"
+#include "ColTrianglePlane.h"
+#include "ColDisk.h"
 
-struct CCollisionData {
+class CCollisionData {
+public:
+    CCollisionData();
+
+public:
     uint16_t m_nNoOfSpheres;
     uint16_t m_nNoOfBoxes;
     uint16_t m_nNoOfTriangles;
@@ -18,45 +26,26 @@ struct CCollisionData {
         uint8_t     m_nNoOfDisks;
     };
     struct {
-        uint8_t bUsesDisks : 1;     // 0x1 - Always set to false
-        uint8_t bHasFaceGroups : 1; // 0x2 - See the huge comment below
-        uint8_t bHasShadowInfo : 1; // 0x4 - See wiki.
+        uint8_t m_useDisksNotLines : 1;     // 0x1 - Always set to false
+        uint8_t m_useColModelSections : 1; // 0x2 - See the huge comment below
+        uint8_t m_hasShadowTris : 1; // 0x4 - See wiki.
     };
     CColSphere* m_pSphereArray;
-    uintptr_t* m_pBoxes;
+    CColBox* m_pBoxes;
     union {
-        CColLine* m_pLines;
-        uintptr_t* m_pDisks;
+        CColLine *m_pLineArray;
+        CColDisk *m_pDiskArray;
     };
 
-    uintptr_t* m_pVertices;
+    CVector* m_pTriCompressedVectorArray;
 
-    // If you take a look here: https://gtamods.com/wiki/Collision_File#Body
-    // You may notice there's an extra section called `TFaceGroups` before `TFace` (triangles)
-    // And it is used in `CCollision::ProcessColModels`.
-    //
-    // The following is only true if `bHasFaceGroups` flag is set (Which is the case only when loaded by `CFileLoader::LoadCollisionModelVer2/3/4`). **
-    // All the data is basically stored in a big buffer, and all the data pointers - except `m_pTrianglePlanes` - point into it.
-    // In case the flag `bHasFaceGroups` is set there's an array of `TFaceGroup` before the triangles, but there's no pointer to it.
-    // In order to access it you have to do some black magic with `pTriangles`. Here's the memory layout of the `TFaceGroups` data:
-    //
-    // FaceGroup[]          - -0x8 - And growing downwards by `sizeof(FaceGroup)` (Which is 28)
-    // uint32 nFaceGroups;  - -0x4
-    // <Triangles>          - FaceGroup data is before the triangles!
-    //
-    // Whenever accessing this section make sure both `CColModel::bSingleAlloc` and `bHasFaceGroups` is set
-    // (also, please, assert if `bHasFaceGroups` is set but `bSingleAlloc` isnt)
-    //
-    // NOTEs:
-    // ** Col models may also be loaded by the Collision plugin from a clump file - In this case `CFileLoader::LoadCollisionModelVer2/3` is called, but then
-    //    the col data is reallocated using `MakeMultipleAlloc` which uses `Copy` to copy the data, in this case the face groups aren't copied. (And the flag is set to false in the ctor)
-    uintptr_t*      m_pTriangles;            // 0x18
-    uintptr_t*      m_pTrianglePlanes;       // 0x1C
-    uint32_t        m_nNumShadowTriangles;   // 0x20
-    uint32_t        m_nNumShadowVertices;    // 0x24
-    uintptr_t*      m_pShadowVertices;       // 0x28
-    uintptr_t*      m_pShadowTriangles;      // 0x2C
-    uint32_t        m_modelSec;
+    CColTriangle*      m_pTriangleArray;            // 0x18
+    CColTrianglePlane*      m_pTrianglePlanes;       // 0x1C
+    int32_t         m_nNoOfShadTriangles;   // 0x20
+    int32_t         m_nNoOfShadTriangleVerts;    // 0x24
+    CVector*        m_pTriCompressedShadVectorArray;       // 0x28
+    CColTriangle*   m_pShadTriangleArray;      // 0x2C
+    int32_t*        m_modelSec;
 };
 static_assert(sizeof(CCollisionData) == 0x34, "Invalid size CCollisionData");
 
