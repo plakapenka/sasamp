c3e0100ccb373ce043f66ca7cd5336ce10b8eab3
plakapenka <plakapenok1@gmail.com>
2023-04-05 14:36:05 +0300

    CColModel

--- app/src/main/cpp/libsamp/game/Collision/ColModel.cpp
+++ app/src/main/cpp/libsamp/game/Collision/ColModel.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "ColModel.h"
+
+CColModel::~CColModel() {
+    if (!m_bIsActive) {
+        return;
+    }
+    RemoveCollisionVolumes();
+}
+
+void CColModel::RemoveCollisionVolumes() {
+//    if (!m_pColData) {
+//        return;
+//    }
+//
+//    //Log("Removing: {} [ColSlot: {}]", LOG_PTR(this), m_nColSlot);
+//
+//    if (m_useSingleAlloc) {
+//        CCollision::RemoveTrianglePlanes(m_pColData);
+//        CMemoryMgr::Free(m_pColData);
+//    } else {
+//        m_pColData->RemoveCollisionVolumes();
+//        delete m_pColData;
+//    }
+//
+//    m_pColData = nullptr;
+}
--- app/src/main/cpp/libsamp/game/Collision/ColModel.h
+++ app/src/main/cpp/libsamp/game/Collision/ColModel.h
@@ -14,8 +14,8 @@ public:
     uint8_t        m_nColSlot;
     union {
         struct {
-            uint8_t m_bHasCollisionVolumes : 1; // AKA `m_bNotEmpty`
-            uint8_t m_bIsSingleColDataAlloc : 1;
+            uint8_t m_hasCollisionVolumes : 1; // AKA `m_bNotEmpty`
+            uint8_t m_useSingleAlloc : 1;
             uint8_t m_bIsActive : 1;
         };
         uint8_t m_nFlags;
@@ -23,6 +23,27 @@ public:
     uint8_t undefined_1;
     uint8_t undefined_2;
     CCollisionData* m_pColData;
+
+public:
+    static void InjectHooks();
+
+    CColModel();
+    ~CColModel();
+
+    static void* operator new(unsigned size);
+    static void operator delete(void* data);
+    CColModel& operator=(const CColModel& colModel);
+
+public:
+    void AllocateData();
+    void AllocateData(int32_t numSpheres, int32_t numBoxes, int32_t numLines, int32_t numVertices, int32_t numTriangles, bool bUsesDisks);
+    void MakeMultipleAlloc();
+    void RemoveCollisionVolumes();
+    void CalculateTrianglePlanes();
+    void RemoveTrianglePlanes();
+
+private:
+    void AllocateData(int32_t size);
 };
 static_assert(sizeof(CColModel) == 0x30, "Invalid");
 
