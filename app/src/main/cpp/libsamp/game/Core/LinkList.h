abbd7a1511600517d2dfaccd7f9c6fcc7b0ec2b5
plakapenka <plakapenok1@gmail.com>
2023-04-05 19:19:12 +0300

    CLinkList

---
+++ app/src/main/cpp/libsamp/game/Core/LinkList.h
@@ -0,0 +1,79 @@
+//
+// Created by plaka on 05.04.2023.
+//
+
+#ifndef LIVERUSSIA_LINKLIST_H
+#define LIVERUSSIA_LINKLIST_H
+
+#include "Link.h"
+
+template <typename T> class CLinkList {
+public:
+    CLink<T>  usedListHead;
+    CLink<T>  usedListTail;
+    CLink<T>  freeListHead;
+    CLink<T>  freeListTail;
+    CLink<T>* links;
+
+//    void* operator new(unsigned size) {
+//        return ((void*(__cdecl*)(uint32))0x821195)(size);
+//    }
+//
+//    void operator delete(void* object) {
+//        ((void(__cdecl*)(void*))0x8213AE)(object);
+//    }
+
+    void Init(int32_t count) {
+        usedListHead.next = &usedListTail;
+        usedListTail.prev = &usedListHead;
+        freeListHead.next = &freeListTail;
+        freeListTail.prev = &freeListHead;
+        links = new CLink<T>[count];
+        for (int32_t i = count - 1; i >= 0; i--) {
+            freeListHead.Insert(&links[i]);
+        }
+    }
+
+    void Shutdown() {
+        delete[] links;
+        links = nullptr;
+    }
+
+    CLink<T>* Insert(T const& data) {
+        CLink<T>* link = freeListHead.next;
+        if (link == &freeListTail)
+            return nullptr;
+        link->data = data;
+        link->Remove();
+        usedListHead.Insert(link);
+        return link;
+    }
+
+    CLink<T>* InsertSorted(T const& data) {
+        CLink<T>* i = nullptr;
+        for (i = usedListHead.next; i != &usedListTail; i = i->next) {
+            if (i->data.m_distance >= data.m_distance)
+                break;
+        }
+        CLink<T>* link = freeListHead.next;
+        if (link == &freeListTail)
+            return nullptr;
+        link->data = data;
+        link->Remove();
+        i->prev->Insert(link);
+        return link;
+    }
+
+    void Clear() {
+        for (CLink<T>* link = usedListHead.next; link != &usedListTail; link = usedListHead.next) {
+            Remove(link);
+        }
+    }
+
+    void Remove(CLink<T>* link) {
+        link->Remove();
+        freeListHead.Insert(link);
+    }
+};
+
+#endif //LIVERUSSIA_LINKLIST_H
