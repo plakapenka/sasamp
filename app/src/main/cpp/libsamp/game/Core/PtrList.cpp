9470a7e224df418d5509f2497e4c6dcee90dc02a
plakapenka <plakapenok1@gmail.com>
2023-04-05 12:09:39 +0300

    CPtrList

--- app/src/main/cpp/libsamp/game/Core/PtrList.cpp
+++ app/src/main/cpp/libsamp/game/Core/PtrList.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "PtrList.h"
+#include "util/patch.h"
+
+CPtrList::CPtrList() {
+    m_node = nullptr;
+}
+
+// Get elements count
+// 0x5521B0
+uint32_t CPtrList::CountElements() const {
+    uint32_t counter;
+    CPtrNode* currNode = GetNode();
+    for (counter = 0; currNode; ++counter) {
+        currNode = currNode->GetNext();
+    }
+    return counter;
+}
+
+// Check if data is a member of list
+// 0x5521D0
+bool CPtrList::IsMemberOfList(void* data) const {
+    CPtrNode* currNode = GetNode();
+    while (currNode) {
+        if (currNode->m_item == data) {
+            return true;
+        }
+        currNode = currNode->m_next;
+    }
+    return false;
+}
--- app/src/main/cpp/libsamp/game/Core/PtrList.h
+++ app/src/main/cpp/libsamp/game/Core/PtrList.h
@@ -6,9 +6,22 @@
 #define LIVERUSSIA_PTRLIST_H
 
 
-class PtrList {
+#include <cstdint>
+#include "PtrNode.h"
 
+class CPtrList {
+public:
+    CPtrNode* m_node;
+
+public:
+    CPtrList();
+
+    uint32_t CountElements() const;
+    bool IsMemberOfList(void* data) const;
+
+    CPtrNode* GetNode() const { return m_node; }
+    bool IsEmpty() const { return !m_node; }
 };
-
+static_assert(sizeof(CPtrList) == 0x4, "Invalid");
 
 #endif //LIVERUSSIA_PTRLIST_H
